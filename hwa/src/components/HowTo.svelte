aa4bab7acce067ed6612457f13620694edae3828
maxdevjs <max.devjs@gmail.com>
2020-05-12 04:04:54 -0300

    Update how to component.

--- hwa/src/components/HowTo.svelte
+++ hwa/src/components/HowTo.svelte
@@ -1,1 +1,10 @@
-<h1>How To</h1>
+
+<div bp="grid">
+  <img bp="offset-5@md 4@md 12@sm" src="img/handwash.gif" alt="How to wash your hands.">
+</div>
+
+<style>
+  img {
+    max-width: 100%;
+  }
+</style>
