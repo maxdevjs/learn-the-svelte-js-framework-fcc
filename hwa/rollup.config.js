88b8aa98828e183aab509c96cfe4d7278faabd12
maxdevjs <max.devjs@gmail.com>
2020-05-12 03:17:45 -0300

    Install blueprint layout library.
    Install also relative rollup plugin.

--- hwa/rollup.config.js
+++ hwa/rollup.config.js
@@ -3,6 +3,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -24,6 +25,9 @@ export default {
 				css.write('public/build/bundle.css');
 			}
 		}),
+
+		// rollup-plugin-css-only
+		css({ output: 'extra.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
