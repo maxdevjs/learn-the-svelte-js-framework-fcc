6131e4968b3d62bc704b52d9cab50b81320a634f
maxdevjs <max.devjs@gmail.com>
2020-05-12 04:05:26 -0300

    Add attribution links.

--- hwa/src/App.svelte
+++ hwa/src/App.svelte
@@ -1,7 +1,19 @@
 <script>
-	import HowTo from './components/HowTo.svelte'
 	import Timer from './components/Timer.svelte'
+	import HowTo from './components/HowTo.svelte'
 </script>
+
+<h1>Handwashing App</h1>
 
 <Timer />
 <HowTo />
+<h3>
+	<a href="https://www.who.int/gpsc/clean_hands_protection/en/">Picture Source</a> | 
+	<a href="https://freesound.org/people/metrostock99/sounds/345086/">Sound Source</a>
+</h3>
+
+<style>
+	h1, h3 {
+		text-align: center;
+	}
+</style>
