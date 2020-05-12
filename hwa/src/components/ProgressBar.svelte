5826260369436d5f6414d83159e77bb4c828fe8f
maxdevjs <max.devjs@gmail.com>
2020-05-12 04:19:33 -0300

    Implement progess bar component.

--- hwa/src/components/ProgressBar.svelte
+++ hwa/src/components/ProgressBar.svelte
@@ -1,1 +1,18 @@
-<h1>Progress Bar</h1>
+<div bp="grid">
+  <div bp="offset-5@md 4@md 12@sm" class="progress-container">
+    <div class="progress-bar" style="width: 20%;">
+      <span class="sr-only">%</span>
+    </div>
+  </div>
+</div>
+
+<style>
+  .progress-container {
+    margin: 20px 0;
+    border: solid rgb(190, 190, 190) 1px;
+  }
+  .progress-bar {
+    height: 30px;
+    background-color: rgb(77, 199, 55);
+  }
+</style>
